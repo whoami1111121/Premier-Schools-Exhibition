081384d40cb949717fc5c70faf32ce90ace4be90
whoami1111121 <udud0099@gmail.com>
2026-06-27 10:07:36 +0545

    nav bar finish

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ const header = document.querySelector(".header");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 20) {
-    header.classList.add("header--scrolled");
+    header.classList.add("header_scrolled");
   } else {
-    header.classList.remove("header--scrolled");
+    header.classList.remove("header_scrolled");
   }
 });
