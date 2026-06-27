4ebe2a9ebc077f79f65e9e6067715e4689ed015f
whoami1111121 <udud0099@gmail.com>
2026-06-27 20:02:08 +0545

    hero sec finish

--- js/main.js
+++ js/main.js
@@ -7,3 +7,21 @@ window.addEventListener("scroll", () => {
     header.classList.remove("header_scrolled");
   }
 });
+
+// const img = document.querySelector(".hero_img img");
+
+// img.onload = () => {
+//   const distance = img.offsetHeight - img.parentElement.offsetHeight;
+
+//   img.animate(
+//     [
+//       { transform: "translateY(0)" },
+//       { transform: `translateY(-${distance}px)` },
+//     ],
+//     {
+//       duration: 12000,
+//       iterations: Infinity,
+//       easing: "linear",
+//     },
+//   );
+// };
